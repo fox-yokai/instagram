ecd3a4a1d3f6e5543b16f9b978a41f8848a94264
fox-yokai <pat.w.cain@gmail.com>
2021-07-19 13:48:31 -0500

    skeleton folders structure created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        
+        <h2>Hellow world!</h2>
       </main>
 
       <footer>
